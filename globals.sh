b0897dd2af64082b1e780ba2f06443662241dc9c
Unknown <tompe97@tlen.pl>
2019-03-14 00:15:35 +0000

    [ADD] - Global variables

---
+++ globals.sh
@@ -0,0 +1,13 @@
+TEMP_REMOTE_DIR=".tmp-domain-package"
+
+# Default Globals
+LOCAL_DIR=''
+LOCAL_MYSQL_USER=''
+LOCAL_MYSQL_PASS=''
+
+REMOTE_HOST=''
+REMOTE_SSH_USER='' # Password will have to be entered upon script start bu user.
+REMOTE_DOMAIN=''
+REMOTE_MYSQL_USER=''
+REMOTE_MYSQL_PASS=''
+REMOTE_MYSQL_DB=''
